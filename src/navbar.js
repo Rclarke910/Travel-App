bbd9fe1d921293f37abb2b1e9b0b2e8d8f80acf3
Rashawn Clarke <102108419+Rclarke910@users.noreply.github.com>
2022-10-07 16:54:57 -0400

    updated logo and navbar

--- src/navbar.js
+++ src/navbar.js
@@ -3,8 +3,8 @@ import React from "react";
 function Navbar(){
     return (
         <div className = "navbar">
-            <img src = "travel-logo.jpeg" className = "logo"></img>
-            <h2 className = "title">My Travel Journal.</h2>
+            <img src = "plane-logo.png" className = "logo"></img>
+            <h2 className = "title">My Travel Journal</h2>
         </div>
     )
 }
